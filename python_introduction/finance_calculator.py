d5cc7dabf2c9a9c19be3566c6ca0dfab8c9973c7
YordanosT1 <jordan.esh88@gmail.com>
2025-05-26 00:04:34 +0300

    My commit to add my work for advance task, 5 Personal Finance Calculator.4

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income:"))
-expense = int(input("Enter your total monthly expenses:"))
-monthly_savings = monthly_income - monthly_expense
+monthly_income = int(input("Enter your monthly income:"))
+monthly_expense = int(input("Enter your total monthly expenses:"))
+monthly_savings = float(monthly_income - monthly_expense)
 projected_savings = int((monthly_savings * 12) + (monthly_savings * 12 * 0.05))
 print("Your monthly savings are", monthly_savings,  end='')
 print(".")
