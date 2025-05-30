41a7054319ba9060076c410adcc6a2073b246677
YordanosT1 <jordan.esh88@gmail.com>
2025-05-30 06:02:33 +0300

    My commit to add my work for task 2: Multiplication Table Generator, used placeholders for a string one wants to insert value

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -6,4 +6,4 @@ number = int(input("Enter a number to see its multiplication table:"))
 #Generate and Print the Multiplication Table
 for i in range(1, 11):
     table = number * i
-    print(number, "*", i, "=", table)
+    print(f"{number} * {i} = {table}")
