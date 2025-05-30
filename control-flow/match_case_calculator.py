8907817bf9c91657a55b266bd90f586ad3667bcb
YordanosT1 <jordan.esh88@gmail.com>
2025-05-30 05:14:31 +0300

    My commit to add my work for task 1:  Simple Calculator with Match Case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,33 @@
+#Task 1. Simple Calculator with Match Case
+
+#propt user to enter the 2 numbers and operations they want to be excuted (calculated).
+num1 = int(input(" Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+operations = str(input("Choose the operation (+, -, *, /):"))
+
+#calculator
+addition = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+
+
+#Performing the Calculation Using Match Case
+match operations:
+    case "+":
+          print("The result is", addition, end='')
+          print(".")
+    case "-":
+          print("The result is", subtract, end='')
+          print(".")
+    case "*":
+          print("The result is", multiply, end='')
+          print(".")
+    case "/":
+            if num2 == 0:
+                  print("Cannot divide by zero")
+            else:
+                  divide = num1 / num2
+                  print("The result is", divide, end='')
+                  print(".")
+    case _:
+            print("syntax error")
