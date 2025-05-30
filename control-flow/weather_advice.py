97c35156ce81ac132f4c0a50f3995532881c8324
YordanosT1 <jordan.esh88@gmail.com>
2025-05-30 04:45:24 +0300

    My commit to add my work for task 0: Weather Recommendation Program, correction used the virable weather instead of user_response

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
 #0. Weather Recommendation Program
-user_response = str(input("What's the weather like today? (sunny/rainy/cold):"))
-if user_response == "sunny":
+weather = input("What's the weather like today? (sunny/rainy/cold):")
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif user_response == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif user_response == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
