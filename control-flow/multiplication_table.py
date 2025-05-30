2e7d61ffd7e770040f656aad48fb33fb9de26b53
YordanosT1 <jordan.esh88@gmail.com>
2025-05-30 05:50:08 +0300

    My commit to add my work for task 2: Multiplication Table Generator

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,9 @@
+#Task 2. Multiplication Table Generator
+
+#Prompt User for a Number
+number = int(input("Enter a number to see its multiplication table:"))
+
+#Generate and Print the Multiplication Table
+for i in range(1, 11):
+    table = number * i
+    print (number, "*", i, "=", table)
