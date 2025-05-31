8c41f26afe58e3f987ad64e5d3117a8cf70a7f7e
YordanosT1 <jordan.esh88@gmail.com>
2025-06-01 02:17:16 +0300

    My commit to add my work for Task 4. Personal Daily Reminder, edited virable name for the checker

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,7 +1,7 @@
 #Task 4. Personal Daily Reminder
 task = str(input("Enter your task:"))
 priority = str(input("Priority (high/medium/low)"))
-time_bound = str(input("Is it time-bound? (yes/no)"))
+time-bound = str(input("Is it time-bound? (yes/no)"))
 
 match priority:
     case "high":
