3cea938b4a7e54b26f83744fb82df84108493468
YordanosT1 <jordan.esh88@gmail.com>
2025-06-07 06:19:49 +0300

    Commit to add arithmetic_operations.py, task 0 Arithmetic Operations Function, use return and remove print

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,20 +3,20 @@ def perform_operation(num1, num2, operation):
     match operation:
         case "add":
             result = num1 + num2
-            print(result)
+            return result
         case "subtract":
             result = num1 - num2
-            print(result)
+            return result
         case "multiply":
              result = num1 * num2
-             print(result)
+             return result
         case "divide":
             if num2 == 0:
                 result = "Can't be devided"
-                print(result)
+                return result
             elif num2 > 0 or num2 <0:
                 result = num1 / num2
-                print(result)
+                return result
 perform_operation
 
     
