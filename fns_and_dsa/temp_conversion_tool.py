fe63ce0628c66cbb722dd26dd7e5b86a4915fdce
YordanosT1 <jordan.esh88@gmail.com>
2025-06-08 02:55:44 +0300

    My commit to push my for Task 3, 3. Temperature Conversion Tool, removed all code and re-writeable.

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,8 +3,6 @@ FAHRENHEIT_TO_CELSIUS_FACTOR =  (5/9)
 CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
 
 temp = input("Enter the temperature to convert: ")
-cf = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
-
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
