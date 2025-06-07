25f55c028c55dd2e386f2f107b2f9e8a295f1601
YordanosT1 <jordan.esh88@gmail.com>
2025-06-08 02:12:29 +0300

    My commit to push my wotk for task 2. Explore  Module, changed prompt statement

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -11,7 +11,7 @@ display_current_datetime()
 
 def calculate_future_date(): #Create a function with a name calculate_future_date
     current_date = datetime.datetime.now() # Get the current date and time
-    future_date = int(input("Enter the future_date: ")) #Prompt the user to enter a number of days (as an integer).
+    future_date = int(input("Enter the number of days to add to the current date:")) #Prompt the user to enter a number of days (as an integer).
     time_delta = timedelta(days=future_date) #timedelta: This object represents a duration. You can specify days, hours, minutes, seconds, weeks, etc.
     future_date = current_date + time_delta #future date
     formatted_date = future_date.strftime("%Y-%m-%d")
