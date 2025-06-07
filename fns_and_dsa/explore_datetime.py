d99e5a356cde1a6b281eaeb60784511bf2bb0ac6
YordanosT1 <jordan.esh88@gmail.com>
2025-06-08 02:10:34 +0300

    My commit to push my wotk for task 2. Explore  Module, made the current dates format in Y-M-D H M S and make  future date have Y-M-D

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,8 +3,10 @@ import datetime
 from datetime import timedelta
 
 def display_current_datetime():
-     current_date = datetime.datetime.now() # Get the current date and time
-     print(current_date) # Print the current date and time
+    current_date = datetime.datetime.now() # Get the current date and time
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S") 
+    print(f"Current date and time: {formatted_date}")
+    # print(current_date) # Print the current date and time
 display_current_datetime()
 
 def calculate_future_date(): #Create a function with a name calculate_future_date
@@ -12,5 +14,7 @@ def calculate_future_date(): #Create a function with a name calculate_future_dat
     future_date = int(input("Enter the future_date: ")) #Prompt the user to enter a number of days (as an integer).
     time_delta = timedelta(days=future_date) #timedelta: This object represents a duration. You can specify days, hours, minutes, seconds, weeks, etc.
     future_date = current_date + time_delta #future date
-    print("Future Date:", future_date) #printing the futur date
+    formatted_date = future_date.strftime("%Y-%m-%d")
+    print(f"Future Date: {formatted_date}")
+    #print("Future Date:", future_date) #printing the futur date
 calculate_future_date()
