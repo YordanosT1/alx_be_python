7e86e82d43aac5bad418a7f9ffb9bdb8c015e63c
YordanosT1 <jordan.esh88@gmail.com>
2025-06-15 00:10:41 +0300

    My commit to add my code and main file for task 0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+#Task 0. Create a Simple Bank Account Class
+#create class
+class  BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance #atribute defined
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if self.balance >= amount:
+                self.balance -= amount
+                return True
+            else:
+                return False
+        else:
+            print("Invalid withdrawal amount.")
+
+    def display_balance(self):
+        print(f"Current balance: ${self.balance}")
+
