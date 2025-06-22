11bc7cdf47478f3372f435c2b13cffd2662a7cc6
YordanosT1 <jordan.esh88@gmail.com>
2025-06-22 21:33:01 +0300

    My commit to add task1: Mastering Inheritance and Composition in Python, correction: return values for ebook and PrintBook corrected and rmoved int and replaced it with str

--- oop/library_system.py
+++ oop/library_system.py
@@ -18,15 +18,20 @@ class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
-    def __int__(self):
-        return f"EBook: {self.file_size}"
- #Set up the class library
+
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
-    def __int__(self):
-        return f"PrintBook: {self.page_count}"
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+
+ #Set up the class library
 class Library:
     def __init__(self):
         self.books = []
