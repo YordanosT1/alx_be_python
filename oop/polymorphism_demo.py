40377d708a004c32b6a2e5baf11eaab915cbb03c
YordanosT1 <jordan.esh88@gmail.com>
2025-06-22 21:48:23 +0300

    My commit to add main file task to and add my work for task 2: Exploring Polymorphism and Method Overriding

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,29 @@
+#Task 2. Exploring Polymorphism and Method Overriding
+'''
+You are tasked with creating a Python script named polymorphism_demo.py. 
+In this script, define a base class Shape with a method area() and derived classes 
+Rectangle and Circle, each overriding the area() method to calculate their respective areas.
+'''
+import math
+
+#Create base class called Shape
+class Shape:
+    def area(self):
+        return f"NotImplementedError"
+
+#Creating derived class: Rectangle
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    
+    def area(self):
+        return self.length * self.width
+
+#Creating derived class: Circle
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius * self.radius
